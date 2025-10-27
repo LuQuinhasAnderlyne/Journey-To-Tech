e00b053ba5e03c9c1a603bd22c548accad03b825
LuQuinhasAnderlyne <lionrx076@gmail.com>
2025-10-26 22:29:37 -0300

    Add files via upload

---
+++ 📕 Projetos/📂 ProgramçãoEmC/🗂️ Mini Projetos Em C/1º Projeto.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    
+    int x = 10, y = 40, z = 310;
+    int soma = x + y;
+    
+    printf("Coordenadas:\n");
+    printf("X (Longitude) = %d\n" , x);
+    printf("Y (Latitude) = %d\n" , y);
+    printf("Z (Altitude)  = %d\n" , x);
+    printf("A soma da Longitude com a Latitude é: X + Y = %d\n" , soma);
+   return 0; 
+
+}
+
+
---
+++ 📕 Projetos/📂 ProgramçãoEmC/🗂️ Mini Projetos Em C/2º Projeto.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    
+    char nome[6];
+    int idade;
+    float altura;
+    
+    printf(" Digite seu Nome, ");
+    scanf(" %s " , nome);
+    printf("Digite sua Idade e ");
+    scanf(" %i " , &idade);
+    printf("Digite sua Altura ");
+    scanf(" %f " , &altura);
+    
+    printf("\nNome: %s\nIdade: %d\nAltura: %.2f\n", nome, idade, altura);
+    return 0;
+
+}
+
+
